a6662f0d848e493f3819faebc63b9a999ada842c
Javier Lopez <javier.lopez@wuaki.tv>
2014-12-03 11:49:38 +0100

    fix several issues on nginx install

--- install_nginx.sh
+++ install_nginx.sh
@@ -4,6 +4,7 @@ echo "Installing nginx"
 sudo apt-get install nginx
 
 echo "Configuring nginx"
-sudo cp -T nginx/hasselhoff.conf /etc/nginx/sites-available/hasselhoff.conf¬
-sudo ln -sT /etc/nginx/sites-available/hasselhoff.conf /etc/nginx/sites-enabled/hasselhoff.conf¬
-sudo /etc/init.d/nginx restart¬
+sudo rm /etc/nginx/sites-enabled/default
+sudo cp -T nginx/hasselhoff.conf /etc/nginx/sites-available/hasselhoff.conf
+sudo ln -sT /etc/nginx/sites-available/hasselhoff.conf /etc/nginx/sites-enabled/hasselhoff.conf
+sudo /etc/init.d/nginx restart
